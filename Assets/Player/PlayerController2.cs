ca22c08ebc706453e30eaff237469b8b37aa01e9
networkSocerer <gigattfyhig@gmail.com>
2025-06-26 14:27:09 +0900

    유니티 재미있는데 ...... 오히려 여기가 인공지능이 끼기 힘들지 않을까 싶기도 하고 ......

--- Assets/Player/PlayerController2.cs
+++ Assets/Player/PlayerController2.cs
@@ -1,6 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class PlayerController2
+public class PlayerController2 : MonoBehaviour
 {
-    
+    Rigidbody2D rbody;
+    float axisH = 0.0f;
+
+    void Start()  // ✅ 오타 수정
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        axisH = Input.GetAxis("Horizontal"); // ← 이 코드도 필요할 수 있음
+    }
+
+    void FixedUpdate()
+    {
+        rbody.linearVelocity = new Vector2(axisH * 3.0f, rbody.linearVelocity.y); // ⚠ 'linearVelocity'도 오타입니다.
+    }
 }

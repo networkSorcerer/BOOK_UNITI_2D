dcb0431920071e975d23243a5830c4e28667b541
networkSocerer <gigattfyhig@gmail.com>
2025-06-26 16:40:08 +0900

    유니티 재미있는데 ..... 컴터가 너무 힘들어 하는게 느껴진다 . ㄷㄷ 이렇게 랙 심하게 걸리는 거 처음인듯 .........

--- Assets/Player/PlayerController2.cs
+++ Assets/Player/PlayerController2.cs
@@ -6,19 +6,33 @@ public class PlayerController2 : MonoBehaviour
 {
     Rigidbody2D rbody;
     float axisH = 0.0f;
+    public float speed = 3.0f;
 
-    void Start()  // ✅ 오타 수정
+    void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        axisH = Input.GetAxis("Horizontal"); // ← 이 코드도 필요할 수 있음
+        axisH = Input.GetAxis("Horizontal");
+
+        if (axisH > 0.0f)
+        {
+            // 오른쪽 이동
+            Debug.Log("오른쪽 이동");
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+        else if (axisH < 0.0f)
+        {
+            // 왼쪽 이동
+            Debug.Log("왼쪽 이동");
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
     }
 
     void FixedUpdate()
     {
-        rbody.linearVelocity = new Vector2(axisH * 3.0f, rbody.linearVelocity.y); // ⚠ 'linearVelocity'도 오타입니다.
+        rbody.linearVelocity = new Vector2(speed * axisH, rbody.linearVelocity.y);
     }
 }
